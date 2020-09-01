0632b58b4209f9c896344a9c780e02eb76469ede
Razvy Cretu <razvan.cretu97@gmail.com>
2020-09-01 22:53:08 +0300

    Hi deploy

--- api/categories/models/categories.js
+++ api/categories/models/categories.js
@@ -1,8 +1,26 @@
-'use strict';
+"use strict";
 
 /**
- * Read the documentation (https://strapi.io/documentation/v3.x/concepts/models.html#lifecycle-hooks)
+ * Read the documentation (https://strapi.io/documentation/3.0.0-beta.x/concepts/models.html#life-cycle-callbacks)
  * to customize this model
  */
 
-module.exports = {};
+const slugify = require("slugify");
+
+module.exports = {
+  /**
+   * Triggered before user creation.
+   */
+  lifecycles: {
+    async beforeCreate(data) {
+      if (data.name) {
+        data.slug = slugify(data.name, { lower: true });
+      }
+    },
+    async beforeUpdate(params, data) {
+      if (data.name) {
+        data.slug = slugify(data.name, { lower: true });
+      }
+    },
+  },
+};
